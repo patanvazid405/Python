5c9bd1c52acf24e65fb795adc98914e01cc55192
Patan Vazid <143118158+patanvazid405@users.noreply.github.com>
2024-02-17 22:25:48 +0530

    Add files via upload

---
+++ loops/looping.py
@@ -0,0 +1,10 @@
+#loops 
+
+# for i in range(0,10):
+#print(i)
+
+k=90
+while k<=100:
+    print("these are :" ,k)
+    k+=1
+   
---
+++ loops/loops.py
@@ -0,0 +1,107 @@
+# # #loops
+
+# i=1
+# while i<=20:
+#   print(i)
+#   i+=1
+#   print("loop ended")  
+
+# # #practise
+# j=1
+# while i<=100:
+#     print(i)
+#     i+=1
+
+# # #3
+# n=1
+# while n <=10:
+#      print(3*n)  
+#      n+=1
+# #3
+# # x=int(input("enter your number: "))
+# # o=1
+# # while o<=10:
+# #     print(x*o)
+ #     o+=1
+
+    #python life
+
+# list=[1,4,9,16,25,67,8]
+# idx=0
+# while idx < len(list):
+#     print(list[idx])
+#     idx+=1
+    
+
+# nums=(12,123,445,655,453,566)  
+
+# x=12
+# i=0
+# while i < len(nums):
+#     if(nums[i]==x):
+#         print("the number is found at",i)
+#         break
+#     else:
+#         print("not found") ; 
+# i += 1  
+
+# l=1
+# while l<=10:
+#     if(l%2 != 0):
+#         l +=1
+#         continue #skip
+#     print(i)
+#     l+=1
+
+# #for loop
+#     str=["vazid bhai","kaya bhai","kaise hoo"]
+
+#     for char in str :
+#         print(char)
+
+#     tup=(12,1,243,442,231)    
+#     num=2
+#     for num in tup:
+#         if (num==1):
+#             print(" is founded",num)
+#             break
+#         print(num)
+#     else:
+#         print("not found")
+
+#practise
+loop=[1,4,9,16,25,36,49,64,81,100]
+for  el in loop:
+     print(el)
+
+#2
+ap=[1,4,9,16,25,36,49,64,81,100]
+x=25
+idx=0
+for el in ap :
+     if(el==x) :
+          print("you are number is",idx)
+          idx+=1 
+
+ 
+# n=5
+# fact=1
+
+# for i in range(n,n+1):
+#      fact*=i
+#      print("factoril of numbers",fact);
+
+k=10
+sum=1
+i=0
+while i<=k:
+     sum*=i
+     i+=1
+     print("total sum",sum)
+
+
+
+
+
+
+ 
